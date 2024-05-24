e56c4e9894e8965581259642cc6e26cb1848fa4e
Sumeet Shrivastava <sumeetshrivas@yahoo.com>
2024-05-24 15:05:07 -0400

    Changes Finish

--- app/src/main/java/com/example/frenchteacherapp/MainActivity.java
+++ app/src/main/java/com/example/frenchteacherapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.frenchteacherapp;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button blackBtn, greenBtn, purpleBtn, redBtn, yellowBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,48 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        blackBtn = findViewById(R.id.blackBtn);
+        greenBtn = findViewById(R.id.greenBtn);
+        purpleBtn = findViewById(R.id.purpleBtn);
+        redBtn = findViewById(R.id.redBtn);
+        yellowBtn = findViewById(R.id.yellowBtn);
+
+        blackBtn.setOnClickListener(this);
+        greenBtn.setOnClickListener(this);
+        purpleBtn.setOnClickListener(this);
+        redBtn.setOnClickListener(this);
+        yellowBtn.setOnClickListener(this);
+
+        blackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.black);
+                mediaPlayer.start();
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int clickedBtnId = v.getId();
+
+        if(clickedBtnId == R.id.blackBtn){
+            playSounds(R.raw.black);
+        } else if(clickedBtnId == R.id.greenBtn){
+            playSounds(R.raw.green);
+        } else if(clickedBtnId == R.id.purpleBtn){
+            playSounds(R.raw.purple);
+        } else if(clickedBtnId == R.id.redBtn){
+            playSounds(R.raw.red);
+        } else if(clickedBtnId == R.id.yellowBtn){
+            playSounds(R.raw.yellow);
+        }
+    }
+
+    public void playSounds(int id) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, id);
+        mediaPlayer.start();
     }
 }
